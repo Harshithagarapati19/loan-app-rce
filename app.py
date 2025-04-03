77d7036bfdae1a16ac27fd33bac64787160b5d93
Harshithagarapati19 <harshithagarapati00@gmail.com>
2025-04-03 12:07:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlist as set
+import joblib
+clf=joblib.load('loan_data1 (3).joblib')
+g=st.number_input('Enter Gender 0:Male 1:Female')
+m=st.number_input('Enter Martial status 0:no 1:yes')
+ai=st.number_input('Enter Applicant Income in thousands')
+la=st.nnumber_input('Enter Loan amount in thousands')
+if st.button('PREDICT'):
+    prediction=clf.predict([[g,m,aif,la]])
+    if prediction=='y':
+        st.text('Loan is approved')
+    else:
+        st.text('Loan is rejected')
